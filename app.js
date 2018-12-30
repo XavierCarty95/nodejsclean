2ec1b06b4a02afe014000f54f1d106bb1fdf6d8f
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-30 20:02:11 +0000

    clean fucntions of node.js

---
+++ app.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+    console.log("three seconds have passed my man")
+    console.log(__dirname);
+   console.log(__filename)
+},2000)
+
+console.log(__dirname);
+console.log(__filename)
